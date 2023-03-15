5b34a78c73c2fad5d092079e75e5d005cc6283e0
harshani457 <harshanichathurangika457@gmail.comm>
2023-03-16 02:43:09 +0530

    01

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Main {
+
+    protected static WebDriver webdriver;
+
+    @BeforeTest
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/driver/chromedriver.exe");
+        ChromeOptions options=new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        webdriver = new ChromeDriver(options);
+    }
+
+    @Test
+    public void btest(){
+
+    }
+}
